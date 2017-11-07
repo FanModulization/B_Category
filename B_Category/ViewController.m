6a019277d0fb7fd005902debe12f10ff612bb80e
fanwise <fanwise.901@gmail.com>
2017-11-07 16:18:54 +0800

    Initial Commit

---
+++ B_Category/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  B_Category
+//
+//  Created by Wei Fan on 07/11/2017.
+//  Copyright © 2017 Wei Fan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
